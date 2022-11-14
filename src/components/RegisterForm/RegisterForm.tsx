9630633c8b6cfb023fc6c4a269c511f933cc6ceb
Núria Ramoneda <nuriarai@gmail.com>
2022-11-14 19:37:35 +0100

    Add functionality to form

--- src/components/RegisterForm/RegisterForm.tsx
+++ src/components/RegisterForm/RegisterForm.tsx
@@ -1,27 +1,67 @@
+import React, { useState } from "react";
+import { UserRegisterData } from "../../types/types.js";
+import useApi from "../../hooks/useApi";
 import RegisterFormStyled from "./RegisterFormStyled";
 
-export interface RegisterFormData {
-  username: string;
-  password: string;
-  email: string;
-}
+const initialFormData = {
+  username: "",
+  password: "",
+  email: "",
+};
 
 const RegisterForm = (): JSX.Element => {
+  const { registerUserApi } = useApi();
+
+  const [formData, setFormData] = useState(initialFormData);
+
+  const handleFormChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData({
+      ...formData,
+      [event.target.id]: event.target.value,
+    });
+  };
+
+  const handleSubmit = (event: React.SyntheticEvent) => {
+    event.preventDefault();
+
+    const formDataToSubmit: UserRegisterData = {
+      username: formData.username,
+      password: formData.password,
+      email: formData.email,
+    };
+
+    registerUserApi(formDataToSubmit);
+  };
+
   return (
     <>
       <h1>Create an account</h1>
-      <RegisterFormStyled className="register">
+      <RegisterFormStyled
+        className="register"
+        onSubmit={(event) => handleSubmit(event)}
+      >
         <div className="register__wrap-input">
           <label htmlFor="username">Username</label>
-          <input type="text" id="username" required />
+          <input
+            type="text"
+            id="username"
+            required
+            onChange={handleFormChange}
+          />
         </div>
         <div className="register__wrap-input">
           <label htmlFor="password">Password</label>
-          <input type="password" id="password" required minLength={8} />
+          <input
+            type="password"
+            id="password"
+            required
+            minLength={8}
+            onChange={handleFormChange}
+          />
         </div>
         <div className="register__wrap-input">
           <label htmlFor="email">Email</label>
-          <input type="email" id="email" required />
+          <input type="email" id="email" required onChange={handleFormChange} />
         </div>
         <div className="register__wrap-input">
           <label htmlFor="picture">Picture</label>
