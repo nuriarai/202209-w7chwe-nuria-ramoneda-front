0409ad20f71a6370abd258a0e99be3b1f899b602
Núria Ramoneda <nuriarai@gmail.com>
2022-11-16 22:45:05 +0100

    Add Login and Register pages

---
+++ src/pages/RegisterPage.tsx
@@ -0,0 +1,12 @@
+import RegisterForm from "../components/RegisterForm/RegisterForm";
+
+const RegisterPage = (): JSX.Element => {
+  return (
+    <>
+      <h1>Register to Llambordaires</h1>
+      <RegisterForm />
+    </>
+  );
+};
+
+export default RegisterPage;
